24367b3786f5c61c9d9fa9b705938f0b04275931
diederikb <beckersdiederik@gmail.com>
2023-03-29 21:41:25 -0700

    working wagner_jones environment

--- aero_gym/envs/wagner_jones.py
+++ aero_gym/envs/wagner_jones.py
@@ -5,19 +5,8 @@ import numpy as np
 from scipy import signal
 from io import StringIO
 from contextlib import closing
-import sys
 
-#TODO: truncate action if it would bring the state outside the observation space
-#TODO: check_env -> reset() check initialize state[1]
-
-def random_fourier_series(t, T, N):
-    A = np.random.normal(0, 1, N)
-    #s = A[0]/2*np.ones(len(t))
-    #A = np.random.randint(-1, high=2, size=N)
-    #s = 0.1*A[0]*np.ones(len(t))
-    s = 0.0
-    s += 0.01*sum(np.sin(2*math.pi/T*n*t)*A[n]/n for n in range(1, N))
-    return s
+#TODO: use observation wrappers (with dicts) instead of observe_wake and observe_h_ddot
 
 class WagnerJonesEnv(gym.Env):
     """
@@ -39,21 +28,18 @@ class WagnerJonesEnv(gym.Env):
                  num_discrete_actions=3,
                  delta_t=0.1,
                  t_max=100.0,
-                 h_ddot_mean=0.0,
-                 h_ddot_std=1.0,
-                 h_ddot_N=100,
-                 h_ddot_T=100,
+                 h_ddot_prescribed=None,
+                 h_ddot_generator=None,
                  rho=1.0,
                  U=1.0,
                  c=1,
                  a=0,
-                 h_ddot_prescribed=None,
-                 random_ics=False,
-                 random_fourier_h_ddot=False,
                  reward_type=1,
                  observe_wake=True,
                  observe_h_ddot=False,
-                 lift_threshold=0.01):
+                 observe_previous_lift=False,
+                 lift_threshold=0.01,
+                 alpha_ddot_threshold=0.1):
         self.continuous_actions = continuous_actions
         self.num_discrete_actions = num_discrete_actions
         self.delta_t = delta_t  # The time step size of the simulation
@@ -61,26 +47,22 @@ class WagnerJonesEnv(gym.Env):
         if h_ddot_prescribed is not None:
             assert len(h_ddot_prescribed) >= int(t_max / delta_t) + 1, "The prescribed vertical acceleration has not enough entries for the whole simulation (including t=0)"
         self.h_ddot_prescribed = h_ddot_prescribed
-        self.h_ddot_mean = h_ddot_mean
-        self.h_ddot_std = h_ddot_std
-        self.h_ddot_N = h_ddot_N
-        self.h_ddot_T = h_ddot_T
+        self.h_ddot_generator = h_ddot_generator
         self.rho = rho
         self.U = U
         self.c = c
         self.a = a
-        self.random_ics = random_ics
-        self.random_fourier_h_ddot = random_fourier_h_ddot
         self.reward_type = reward_type
         
         self.alpha_eff_threshold = 10 * math.pi / 180
-        self.hddot_threshold = np.inf
+        self.h_ddot_threshold = np.inf
         self.alpha_dot_threshold = 10 * math.pi / 180
-        self.alpha_ddot_threshold = 0.1 #10 * math.pi / 180
+        self.alpha_ddot_threshold = alpha_ddot_threshold
         self.lift_threshold = lift_threshold
 
         self.observe_wake = observe_wake
         self.observe_h_ddot = observe_h_ddot
+        self.observe_previous_lift = observe_previous_lift
 
         # Observations are the wing's effective AOA, the vertical acceleration, the angular velocity and acceleration, and the states for Theodorsen's function approximation. 
         self.state_low = np.array(
@@ -101,24 +83,25 @@ class WagnerJonesEnv(gym.Env):
             ]
         ).astype(np.float32)
          
-        obs_low = self.state_low
-        obs_high = self.state_high
-
         obs_low = self.state_low[0:2]
         obs_high = self.state_high[0:2]
 
         if self.observe_wake:
-            np.append(obs_low, self.state_low[2:])
-            np.append(obs_high, self.state_high[2:])
+            obs_low = np.append(obs_low, self.state_low[2:])
+            obs_high = np.append(obs_high, self.state_high[2:])
         if self.observe_h_ddot:
-            np.append(obs_low, np.float32(self.h_ddot_threshold))
-            np.append(obs_high, -np.float32(self.h_ddot_threshold))
+            obs_low = np.append(obs_low, -np.float32(self.h_ddot_threshold))
+            obs_high = np.append(obs_high, np.float32(self.h_ddot_threshold))
+        if self.observe_previous_lift:
+            obs_low = np.append(obs_low, -np.float32(self.lift_threshold))
+            obs_high = np.append(obs_high, np.float32(self.lift_threshold))
 
         self.observation_space = spaces.Box(obs_low, obs_high, (len(obs_low),), dtype=np.float32)
 
         # We have 1 action: the angular acceleration
         if self.continuous_actions:
-            self.action_space = spaces.Box(-self.alpha_ddot_threshold, self.alpha_ddot_threshold, (1,), dtype=np.float32)
+            # Will be rescaled by the threshold
+            self.action_space = spaces.Box(-1, 1, (1,), dtype=np.float32)
         else:
             self.action_space = spaces.Discrete(self.num_discrete_actions)
         self.discrete_action_values = self.alpha_ddot_threshold * np.linspace(-1, 1, num=self.num_discrete_actions) ** 3
@@ -144,10 +127,14 @@ class WagnerJonesEnv(gym.Env):
             [0, 0],
         ])
         C = np.block([
-            [Dtilde*C2, C1*U, Ctilde]
+            [Dtilde*C2, C1*c/U, Ctilde]
         ])
+        # Only added mass
+        #C = np.block([
+        #    [0, C1*U, np.zeros((1,2))]
+        #])
         D = np.array([
-            [-C1, -C1*a]
+            [-C1*c/U**2, -C1*a*c/U**2]
         ])
         sys = signal.StateSpace(A, B, C, D)
         sys = sys.to_discrete(delta_t)
@@ -172,14 +159,15 @@ class WagnerJonesEnv(gym.Env):
     def _get_obs(self):
         obs = self.state[0:2]
         if self.observe_wake:
-            np.append(obs, self.state[2:])
+            obs = np.append(obs, self.state[2:])
         if self.observe_h_ddot:
-            np.append(obs, self.h_ddot)
+            obs = np.append(obs, self.h_ddot)
+        if self.observe_previous_lift:
+            obs = np.append(obs, self.fy)
         return obs
 
     def _get_info(self):
-        return {"previous lift": self.fy, "time": self.t, "time step": self.time_step, "TimeLimit.truncated"=self.truncated}
-
+        return {"previous fy": self.fy, "previous alpha_ddot": self.alpha_ddot, "current alpha_dot": self.state[1], "current alpha_eff": self.state[0], "current h_ddot": self.h_ddot, "current t": self.t, "current time_step": self.time_step, "TimeLimit.truncated": self.truncated}
 
     def reset(self, seed=None, options=None):
         # We need the following line to seed self.np_random
@@ -191,18 +179,24 @@ class WagnerJonesEnv(gym.Env):
         self.truncated = False
         self.terminated = False
 
-        # Recreate a prescribed vertical acceleration if a random fourier signal is required
-        if self.random_fourier_h_ddot:
-            self.h_ddot_prescribed = self.U * random_fourier_series(np.linspace(0, self.t_max, int(self.t_max / self.delta_t) + 1), self.h_ddot_T, self.h_ddot_N) 
-
-        # If there is no prescribed vertical acceleration, create a random vertical acceleration
-        if self.h_ddot_prescribed is None:
-            self.h_ddot = self.np_random.normal(self.h_ddot_mean, self.h_ddot_std)
+        
+        # If there is no prescribed vertical acceleration use the provided function to generate one
+        if options is not None:
+            if "h_ddot_prescribed" in options:
+                assert len(options["h_ddot_prescribed"]) >= int(self.t_max / self.delta_t) + 1, "The prescribed vertical acceleration has not enough entries for the whole simulation (including t=0)"
+                self.h_ddot_prescribed = options["h_ddot_prescribed"]
+        if self.h_ddot_prescribed is not None:
+            self.h_ddot_list = np.array(self.h_ddot_prescribed, dtype=np.float32)
+        elif self.h_ddot_generator is not None:
+            self.h_ddot_list = np.array(self.h_ddot_generator(self), dtype=np.float32)
         else:
-            self.h_ddot = self.h_ddot_prescribed[self.time_step]
+            self.h_ddot_list = np.zeros(int(self.t_max / self.delta_t + 1), dtype=np.float32)
+            print("No h_ddot provided, using zeros instead")
 
+        self.h_ddot = self.h_ddot_list[self.time_step]
         self.state = np.zeros(len(self.A), dtype=np.float32)
         self.fy = np.float32(0.0)
+        self.alpha_ddot = np.float32(0.0)
 
         observation = self._get_obs()
         info = self._get_info()
@@ -218,24 +212,25 @@ class WagnerJonesEnv(gym.Env):
 
         # Assign action to alpha_ddot
         if self.continuous_actions:
-            alpha_ddot = action[0]
+            self.alpha_ddot = action[0] * self.alpha_ddot_threshold
         else:
-            alpha_ddot = self.discrete_action_values[action] 
+            self.alpha_ddot = self.discrete_action_values[action] 
 
         # Compute the lift and reward
-        u = np.array([self.h_ddot, alpha_ddot], dtype=np.float32)
+        u = np.array([self.h_ddot, self.alpha_ddot], dtype=np.float32)
         CL = (np.dot(self.C, self.state) + np.dot(self.D, u))[0]
         self.fy = 0.5 * CL * (self.U ** 2) * self.c * self.rho
 
         # Compute the reward
+        #reward = 1 - (self.fy / self.lift_threshold) ** 2
         if self.reward_type == 1:
             reward = -(self.fy ** 2) # v1
         elif self.reward_type == 2:
-            reward = -(self.fy ** 2 + 0.1 * alpha_ddot ** 2) # v2
+            reward = -(self.fy ** 2 + 0.1 * self.alpha_ddot ** 2) # v2
+        elif self.reward_type == 3:
+            reward = -abs(self.fy / self.lift_threshold) + 1
         elif self.reward_type == 5:
             reward = -1 * (math.exp((self.fy / self.lift_threshold) ** 2) - 1) + 1 # v5
-        elif self.reward_type == 6:
-            reward = -10 * (self.fy ** 2) + 1 / (self.t_max / self.delta_t) # v6
 
         # Update the time and time step
         self.t += self.delta_t
@@ -244,11 +239,7 @@ class WagnerJonesEnv(gym.Env):
         # Update state
         self.state = np.matmul(self.A, self.state) + np.dot(self.B, u)
 
-        # If there is no prescribed vertical acceleration, create a random vertical acceleration
-        if self.h_ddot_prescribed is None:
-            self.h_ddot = self.np_random.normal(self.h_ddot_mean, self.h_ddot_std)
-        else:
-            self.h_ddot = self.h_ddot_prescribed[self.time_step]
+        self.h_ddot = self.h_ddot_list[self.time_step]
 
         # Check if timelimit is reached or state went out of bounds
         if self.t > self.t_max or math.isclose(self.t, self.t_max, rel_tol=1e-9):
@@ -270,8 +261,6 @@ class WagnerJonesEnv(gym.Env):
     def render(self):
         if self.render_mode == "ansi":
             return self._render_text()
-        else:
-            return self._render_frame()
 
     def _render_text(self):
         outfile = StringIO()
--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup
 setup(
     name="aero_gym",
     version="0.0.1",
-    install_requires=["gymnasium==0.28"],
+    install_requires=[
+        "gymnasium==0.28",
+        "scipy"
+    ],
 )
